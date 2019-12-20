4a9ba68b56ee962e4395f39e98ce7c4973871bea
Andreas Kleber <andreas@drosselweg7a.de>
2019-12-20 16:14:25 +0000

    adapt username

--- influxdb-backup.sh
+++ influxdb-backup.sh
@@ -3,5 +3,5 @@ today=`date '+%Y_%m_%d-%H_%M_%S'`;
 backuppath="/var/backups/influxdb/$today"
 
 /usr/bin/influxd backup -portable $backuppath
-/usr/bin/scp -i /home/pi/.ssh/id_rsa -r $backuppath root@nas.kleber:/volume1/NAS/Backup/rpi3-influxdb
+/usr/bin/scp -i /home/pi/.ssh/id_rsa -r $backuppath backup@nas.kleber:/volume1/NAS/Backup/rpi3-influxdb
 rm -rf $backuppath
